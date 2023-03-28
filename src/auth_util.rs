44f2a6c97484176a818023f35bbd560b18b11f48
technorav3nn <Colin01460@outlook.com>
2023-03-28 13:37:29 -0400

    fix

--- src/auth_util.rs
+++ src/auth_util.rs
@@ -1,16 +1,3 @@
-use std::{fs, path::Path};
-
-fn get_auth_as_bytes() -> &'static [u8] {
-    let file_contents: &'static [u8] = include_bytes!("SWMAuth2");
-    return file_contents;
-}
-
-pub fn save_swm_auth() {
-    let file_contents: &'static [u8] = get_auth_as_bytes();
-    let path = Path::new("SWMAuth2");
-    let display = path.display();
-
-    if let Err(why) = fs::write(path, file_contents) {
-        panic!("couldn't write SWMAuth2 to {}: {}", display, why);
-    };
+pub fn nothing() {
+    println!("Nothing");
 }
